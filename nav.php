78bc2de8d391a7aa201242c1500edfa713dac92d
Nitin Gojiya <nitingojiya2000@gmail.com>
2024-08-18 16:57:51 +0530

    nav and home
    updated navbar and home slider

---
+++ nav.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="style.css" rel="stylesheet">
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+        <header>
+        <div class="hstack gap-3">
+  <div class="p-2"><img src="logo.png" height="40px" width="100px"></div>
+  <div class="p-2 ms-auto pp-2">Home</div>
+  <div class="vr"></div>
+  <div class="p-2 pp-2 ">
+    Menu
+  </div>
+  <div class="vr"></div>
+  <div class="p-2 pp-2">Your order</div>
+  <div class="vr"></div>
+  <div class="p-2 pp-2">Contact</div>
+  <div class="vr"></div>
+  <div class="p-2 pp-2">About us</div>
+  <div class="vr"></div>
+  <div class="p-2 pp-2">feedback</div>
+</div>
+        </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
